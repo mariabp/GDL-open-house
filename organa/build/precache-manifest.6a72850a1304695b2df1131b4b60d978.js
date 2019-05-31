43215e63a91a4561064434096f35f1eb147ac97f
Sandunga <sandra.cuevastorres@gmail.com>
2019-05-30 19:03:33 -0500

    sign in component created and its css file

---
+++ organa/build/precache-manifest.6a72850a1304695b2df1131b4b60d978.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e306fe8fc30497e9b545521dc4be7e",
+    "url": "/GDL-open-house/index.html"
+  },
+  {
+    "revision": "9f512661a63708228837",
+    "url": "/GDL-open-house/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/GDL-open-house/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "9f512661a63708228837",
+    "url": "/GDL-open-house/static/js/main.2e1e38ab.chunk.js"
+  },
+  {
+    "revision": "87f91083688119231ac5",
+    "url": "/GDL-open-house/static/js/runtime~main.4222a562.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GDL-open-house/static/media/logo.5d5d9eef.svg"
+  }
+]);
